17ae1c6c6baf3bb679343de0ce6b4fdba15404f5
fosJoddie <oflatval@gmail.com>
2018-02-12 12:47:21 +0100

    Fix getAddress geocoder

--- app/src/geo/google.js
+++ app/src/geo/google.js
@@ -3,12 +3,11 @@ let config = require('config');
 let inside = require('point-in-polygon');
 let _ = require('lodash');
 let NodeGeocoder = require('node-geocoder');
+
 let geocoder = NodeGeocoder({
     provider: 'google',
     httpAdapter: 'https',
-    apiKey: config.gmaps.key,
-    formatter: 'string',
-    formatterPattern: config.locale.addressformat
+    apiKey: config.gmaps.key
 });
 let getCoords = NodeGeocoder({
     provider: 'google',
@@ -27,10 +26,43 @@ module.exports = {
     },
 
     getAddress: function(location, callback) {
-        geocoder.reverse(location, function(err, res) {
+        geocoder.reverse(location, function(err, data) {
             if (err) log.error(err);
-            log.debug(`Fetched address ${res[0]}`);
-            callback(res[0]);
+
+			var res = {};
+			if(data && data.length > 0) {
+				//we found it
+				res.addr = config.locale.addressformat
+				.replace(/%n/, data[0].streetNumber)
+				.replace(/%S/, data[0].streetName)
+				.replace(/%z/, data[0].zipcode)
+				.replace(/%P/, data[0].country)
+				.replace(/%p/, data[0].countryCode)
+				.replace(/%c/, data[0].city)
+				.replace(/%T/, data[0].state)
+				.replace(/%t/, data[0].stateCode);
+				res.streetNumber = data[0].streetNumber;
+				res.streetName = data[0].streetName;
+				res.zipcode = data[0].zipcode;
+				res.country = data[0].country;
+				res.countryCode = data[0].countryCode;
+				res.city = data[0].city;
+				res.state = data[0].state;
+				res.stateCode = data[0].stateCode;
+			} else {
+				//didn't find anything, default it
+				res.addr = '';
+				res.streetNumber = '';
+				res.streetName = '';
+				res.zipcode = '';
+				res.country = '';
+				res.countryCode = '';
+				res.city = '';
+				res.state = '';
+				res.stateCode = '';
+			}
+
+            callback(err, res);
         });
     },
 
