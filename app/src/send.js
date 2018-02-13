b1a541ec987fb405c25ba820ad313ca36deb618b
KartulUdus <derek.maekask@gmail.com>
2018-02-13 23:23:01 +0200

    check for type of webhook

--- app/src/send.js
+++ app/src/send.js
@@ -8,18 +8,19 @@ module.exports = {
 
     sendHooks: function(queue, data) {
 
-        amqp.connect(config.rabbit.conn, function (err, conn) {
-            conn.createChannel(function (err, ch) {
-                let q = queue;
-                ch.assertQueue(q, {durable: false});
-                ch.sendToQueue(q, new Buffer(JSON.stringify(data)), {persistent: true});
-                log.info("Sent " + queue + " to bunnywabbit");
+        if(queue === 'pokemon' || queue === 'raid' || queue === 'gym-info') {
+            amqp.connect(config.rabbit.conn, function (err, conn) {
+                conn.createChannel(function (err, ch) {
+                    let q = queue;
+                    ch.assertQueue(q, {durable: false});
+                    ch.sendToQueue(q, new Buffer(JSON.stringify(data)), {persistent: true});
+                    log.info("Sent " + queue + " to bunnywabbit");
+                });
+                setTimeout(function () {
+                    conn.close();
+                }, 500);
             });
-            setTimeout(function () {
-                conn.close();
-            }, 500);
-        });
-
+        }
     },
     sendTestHook: function(queue, data, callback) {
 
