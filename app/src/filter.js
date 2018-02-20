9f7a97de1fbcce94d0dd786079be513a38036663
fosJoddie <oflatval@gmail.com>
2018-02-20 12:09:12 +0100

    Support for embed color by rarity for IV alerts

--- app/src/filter.js
+++ app/src/filter.js
@@ -52,6 +52,7 @@ client.on('ready', () => {
                 data.applemap = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`;
                 data.mapurl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`;
                 data.color = monsterData[data.pokemon_id].types[0].color;
+                data.rarityColor =  findRarityColor(data.iv);
                 data.tth = moment.preciseDiff(Date.now(), data.disappear_time * 1000, true);
                 data.distime = moment(data.disappear_time * 1000).format(config.locale.time);
                 data.imgurl = `${config.general.imgurl}${data.pokemon_id}`;
@@ -95,6 +96,7 @@ client.on('ready', () => {
                                         form: data.formname,
                                         imgurl: data.imgurl.toLowerCase(),
                                         color: data.color,
+                                        rarityColor: data.rarityColor,
                                         // geocode stuff
                                         addr: geoResult.addr,
                                         streetNumber: geoResult.streetNumber,
@@ -353,4 +355,28 @@ function sendDMAlarm(message, human, e, map) {
             }
         })
     } else log.warn(`Tried to send message to ID ${human}, but didn't find it`);
+}
+
+
+function findRarityColor(iv) {
+    if(!iv || iv < 25) {
+        //gray / trash / missing
+        return 0x9D9D9D;
+    } else if(iv < 50) {
+        //white / common
+        return 0xFFFFFF;
+    } else if(iv < 82) {
+        // green / uncommon
+        return 0x1EFF00;
+    } else if(iv < 90) {
+        // blue / rare
+        return 0x0070DD;
+    } else if(iv < 100 ) {
+        // purple / epic
+        return 0xA335EE;
+    }
+
+    //it must be perfect
+    // orange / legendary
+    return 0xFF8000;
 }
