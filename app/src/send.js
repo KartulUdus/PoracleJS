c2605ad31bd698b622960ab95caef6784c45b57c
KartulUdus <derek.maekask@gmail.com>
2018-02-23 19:13:02 +0200

    revert latest breaking amqp things

--- app/src/send.js
+++ app/src/send.js
@@ -11,20 +11,20 @@ cache.on('put', function(key, val, ttl) { });
 
 module.exports = {
 
-    sendHooks: function(conn, queue, data) {
+    sendHooks: function(queue, data) {
 
         if(queue === 'pokemon' || queue === 'raid' || queue === 'gym_details') {
             if(queue === 'pokemon'){
                 if(cache.get(data.encounter_id) === undefined){
                     cache.put(data.encounter_id, 'cached');
-                    sendRabbitMQ(conn, queue, data)
+                    sendRabbitMQ(queue, data)
                 } else log.warn(`Encounter ${data.encounter_id} was sent again too fast`)
             }else if(queue === 'raid'){
                 if(cache.get(data.gym_id) === undefined) {
                     cache.put(data.gym_id, 'cachedGym');
-                    sendRabbitMQ(conn, queue, data)
+                    sendRabbitMQ(queue, data)
                 } else log.warn(`Encounter ${data.gym_id} was sent again too fast`)
-            } else sendRabbitMQ(conn, queue, data);
+            } else sendRabbitMQ(queue, data);
         }
     },
     sendTestHook: function(queue, data, callback) {
@@ -41,11 +41,14 @@ module.exports = {
                 conn.close();
             }, 500);
         });
+
     }
+
 };
 
-function sendRabbitMQ(conn, queue, data){
+function sendRabbitMQ(queue, data){
 
+    amqp.connect(config.rabbit.conn, function (err, conn) {
         conn.createChannel(function (err, ch) {
             let q = queue;
             ch.assertQueue(q, {durable: false});
@@ -55,4 +58,5 @@ function sendRabbitMQ(conn, queue, data){
         setTimeout(function () {
             conn.close();
         }, 500);
+    });
 }
