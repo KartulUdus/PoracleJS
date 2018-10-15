f2ce4255145ba588255a6e02ccedfe29dc391d46
KartulUdus <derek.maekask@gmail.com>
2018-10-15 12:36:45 +0300

    check for form === '0'

--- app/src/filter.js
+++ app/src/filter.js
@@ -137,7 +137,7 @@ client.on('ready', () => {
 				data.emoji = monsterData[data.pokemon_id].types.forEach((type) => {
 					e.push(type.emoji);
 				});
-				if (!data.form) {
+				if (!data.form || data.form === '0') {
 					data.formname = formData[data.pokemon_id][data.form];
 					data.imgurl = data.imgurl.concat(`-${data.formname}.png`);
 				}
