d726833b4b71b96a637d7ffab875419623f14649
KartulUdus <derek.maekask@gmail.com>
2018-12-13 16:39:32 +0200

    commit 2

--- src/app/controllers/monster.js
+++ src/app/controllers/monster.js
@@ -1,7 +1,7 @@
 class Monster{
 	discord;
 	db;
-
+	log;
 
 }
 
--- src/app/handlers/receiver.js
+++ src/app/handlers/receiver.js
@@ -4,13 +4,15 @@ const _ = require('lodash')
 const mysql = require('mysql2')
 const config = require('config')
 const db = mysql.createPool(config.db)
-
+const MonsterController = require('../controllers/monster')
+const monsterController = new MonsterController(db, log)
 
 
 
 module.exports =  async (req, reply) => {
-	let type =  req.body[1].type? || :req.body.type
-	switch(type ){
+	let data = req.body
+	if (!Array.isArray(data)) data = [ data ]
+	switch(data[0].type ){
 		case "pokemon":{
 			log.info('pokemon')
 			reply.send({webserver: 'happy'})
@@ -22,6 +24,13 @@ module.exports =  async (req, reply) => {
 			break
 		}
 	}
-	reply.send({webserver: 'happy'})
+
+
+	reply.send(
+		{
+		objecttype: req.body.type,
+		datatype: typeof req.body,
+		array: Array.isArray(req.body),
+		data: data})
 	log.info(req.body)
 }
