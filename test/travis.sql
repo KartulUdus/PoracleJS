198492a8cf429dbf1dd63c3bf4e8116778f727ec
KartulUdus <derek.maekask@gmail.com>
2019-01-07 20:36:15 +0200

    travis

--- test/travis.sql
+++ test/travis.sql
@@ -13,6 +13,12 @@ CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 
 
+# Create DB
+# ------------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS `poracle` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `poracle`;
+
 
 # Dump of table activeRaid
 # ------------------------------------------------------------
