a4bae6c5ffb307e8937aca6685b361d9b07c63ee
KartulUdus <derek.maekask@gmail.com>
2019-09-25 22:17:03 +0300

    updated more raid stuff, multicommands

--- src/lib/discord/commando/commands/raid.js
+++ src/lib/discord/commando/commands/raid.js
@@ -1,4 +1,11 @@
-exports.run = async (client, msg, args) => {
+exports.run = async (client, msg, initialArgs) => {
+	let command
+	if (initialArgs.includes('|')) {
+		command = initialArgs.join(' ').split('|').map(com => com.split(' ').filter(arg => arg))
+	} else {
+		command = [initialArgs]
+	}
+
 	const typeArray = Object.keys(client.utilData.types).map(o => o.toLowerCase())
 	let target = { id: msg.author.id, name: msg.author.tag, webhook: false }
 
@@ -6,7 +13,9 @@ exports.run = async (client, msg, args) => {
 		if (!client.config.discord.admins.includes(msg.author.id) && msg.channel.type === 'text') {
 			return await msg.author.send('Please run commands in Direct Messages')
 		}
-		let webhookName = args.find(arg => arg.match(/name\S+/gi))
+		let webhookName
+		const webhookArray = command.find(args => args.find(arg => arg.match(/name\S+/gi)))
+		if (webhookArray) webhookName = webhookArray.find(arg => arg.match(/name\S+/gi))
 		if (webhookName) webhookName = webhookName.replace('name', '')
 		if (client.config.discord.admins.includes(msg.author.id) && msg.channel.type === 'text') target = { id: msg.channel.id, name: msg.channel.name, webhook: false }
 		if (client.config.discord.admins.includes(msg.author.id) && webhookName) {
@@ -29,86 +38,99 @@ exports.run = async (client, msg, args) => {
 		}
 		if (target.webhook) target.id = isRegistered.id
 
-		const remove = args.find(arg => arg === 'remove')
+		let reaction = '👌'
+		for (args of command) {
 
-		let monsters = []
-		let exclusive = 0
-		let distance = 0
-		let team = 4
-		let template = 1
-		const levels = []
-		let pings = ''
+			
+			const remove = args.find(arg => arg === 'remove')
+			
+			let monsters = []
+			let exclusive = 0
+			let distance = 0
+			let team = 4
+			let template = 1
+			const levels = []
+			let pings = ''
+			
+			const formNames = args.filter(arg => arg.match(/form\S/gi)).map(arg => arg.replace('form', ''))
+			const argTypes = args.filter(arg => typeArray.includes(arg))
+			
+			if (formNames.length) {
+				monsters = Object.values(client.monsters).filter(mon => ((args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString())) && formNames.includes(mon.form.name.toLowerCase())
+				|| mon.types.map(t => t.name.toLowerCase()).find(t => argTypes.includes(t)) && formNames.includes(mon.form.name.toLowerCase())
+				|| args.includes('everything'))	&& formNames.includes(mon.form.name.toLowerCase()))
+			} else {
+				monsters = Object.values(client.monsters).filter(mon => ((args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString())) && !mon.form.id
+				|| mon.types.map(t => t.name.toLowerCase()).find(t => argTypes.includes(t)) && !mon.form.id
+				|| args.includes('everything'))	&& !mon.form.id)
+			}
 
-		const formNames = args.filter(arg => arg.match(/form\S/gi)).map(arg => arg.replace('form', ''))
-		const argTypes = args.filter(arg => typeArray.includes(arg))
+			const genCommand = args.filter(arg => arg.match(/gen[1-7]/gi))
+			const gen = genCommand.length ? client.utilData.genData[+genCommand[0].replace(/gen/gi, '')] : 0
 
-		if (formNames.length) {
-			monsters = Object.values(client.monsters).filter(mon => ((args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString())) && formNames.includes(mon.form.name.toLowerCase())
-			|| mon.types.map(t => t.name.toLowerCase()).find(t => argTypes.includes(t)) && formNames.includes(mon.form.name.toLowerCase())
-			|| args.includes('everything'))	&& formNames.includes(mon.form.name.toLowerCase()))
-		} else {
-			monsters = Object.values(client.monsters).filter(mon => ((args.includes(mon.name.toLowerCase()) || args.includes(mon.id.toString())) && !mon.form.id
-			|| mon.types.map(t => t.name.toLowerCase()).find(t => argTypes.includes(t)) && !mon.form.id
-			|| args.includes('everything'))	&& !mon.form.id)
-		}
+			if (gen) monsters = monsters.filter(mon => mon.id >= gen.min && mon.id <= gen.max)
 
-
-		if (!monsters.length && levels.length) return await msg.reply('404 NO MONSTERS FOUND')
-
-		args.forEach((element) => {
-			if (element === 'ex') exclusive = 1
-			else if (element.match(/level\d{1}/gi)) levels.push(element.match(/level\d{1}/gi)[0].replace(/level/gi, ''))
-			else if (element.match(/template\d{1,4}/gi)) template = element.match(/template\d{1,4}/gi)[0].replace(/template/gi, '')
-			else if (element.match(/d\d{1,8}/gi)) distance = element.match(/d\d{1,8}/gi)[0].replace(/d/gi, '')
-			else if (element.endsWith('ping')) 	pings = pings.concat(element.slice(0, element.length - 'ping'.length))
-			else if (element === 'instinct') team = 3
-			else if (element === 'valor') team = 2
-			else if (element === 'mystic') team = 1
-			else if (element === 'harmony') team = 0
-		})
-
-		if (!remove) {
-			const insert = monsters.map(mon => ({
-				id: `"${target.id}"`,
-				pokemon_id: mon.id,
-				ping: `"${pings}"`,
-				exclusive,
-				template,
-				distance,
-				team,
-				level: 9000,
-				form: mon.form.id,
-			}))
-
-			levels.forEach((level) => {
-				insert.push({
-					id: `"${target.id}"`,
-					pokemon_id: 9000,
-					ping: `"${pings}"`,
-					exclusive,
+			
+			
+			if (!monsters.length && levels.length) return await msg.reply('404 NO MONSTERS FOUND')
+			
+			args.forEach((element) => {
+				if (element === 'ex') exclusive = 1
+				else if (element.match(/level\d{1}/gi)) levels.push(element.match(/level\d{1}/gi)[0].replace(/level/gi, ''))
+				else if (element.match(/template\d{1,4}/gi)) template = element.match(/template\d{1,4}/gi)[0].replace(/template/gi, '')
+				else if (element.match(/d\d{1,8}/gi)) distance = element.match(/d\d{1,8}/gi)[0].replace(/d/gi, '')
+				else if (element.endsWith('ping')) 	pings = pings.concat(element.slice(0, element.length - 'ping'.length))
+				else if (element === 'instinct') team = 3
+				else if (element === 'valor') team = 2
+				else if (element === 'mystic') team = 1
+				else if (element === 'harmony') team = 0
+			})
+			
+			if (!remove) {
+				const insert = monsters.map(mon => ({
+					id: target.id,
+					pokemon_id: mon.id,
+					ping: pings.lenght ? pings : '""',
+					exclusive : !!exclusive,
 					template,
 					distance,
 					team,
-					level,
-					form: 9000,
+					level: 9000,
+					form: mon.form.id,
+				}))
+				
+				levels.forEach((level) => {
+					insert.push({
+						id: target.id,
+						pokemon_id: 9000,
+						ping: pings.lenght ? pings : '""',
+						exclusive : !!exclusive,
+						template,
+						distance,
+						team,
+						level,
+						form: 9000,
+					})
 				})
-			})
 
-			const result = await client.query.insertOrUpdateQuery('raid', insert)
-			result.length ? msg.react('✅') : msg.react('👌')
-		} else {
-			const monsterIds = monsters.map(mon => mon.id)
-			let result = 0
-			if (monsterIds.length) {
-				const monResult = await client.query.deleteWhereInQuery('raid', target.id, monsterIds, 'pokemon_id')
-				result += monResult
+				const result = await client.query.insertOrUpdateQuery('raid', insert)
+				result.length ? reaction = '✅' : reaction
+			} else {
+				const monsterIds = monsters.map(mon => mon.id)
+				let result = 0
+				if (monsterIds.length) {
+					const monResult = await client.query.deleteWhereInQuery('raid', target.id, monsterIds, 'pokemon_id')
+					result += monResult
+				}
+				if (levels.length) {
+					const lvlResult = await client.query.deleteWhereInQuery('raid', target.id, levels, 'level')
+					result += lvlResult
+				}
+				result ? reaction = '✅' : reaction
 			}
-			if (levels.length) {
-				const lvlResult = await client.query.deleteWhereInQuery('raid', target.id, levels, 'level')
-				result += lvlResult
-			}
-			result ? msg.react('✅') : msg.react('👌')
+			
 		}
+		await msg.react(reaction)
 	} catch (err) {
 		client.log.error('raid command unhappy:', err)
 	}
