fd9a4bc7a0140b4e94c7e2941943af45c425cbf1
sabregreen <tharpj@gmail.com>
2020-07-07 10:19:13 -0400

    Double role ping fix (#159)
    * Fix double ping for roles
    
    * Fix double ping for role tagging
    
    * Fix double ping for role tagging
    
    * Fix double ping for role tagging

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -172,10 +172,14 @@ class Quest extends Controller {
 				const mustache = this.mustache.compile(this.translator.translate(template))
 				const message = JSON.parse(mustache(view))
 
-				if (cares.ping) {
-					if (!message.content) message.content = cares.ping
-					if (message.content) message.content += cares.ping
-				}
+                                if (cares.ping) {
+                                        if (!message.content) {
+                                                message.content = cares.ping;
+                                        } else {
+                                                message.content += cares.ping;
+                                        }
+                                }
+
 				const work = {
 					lat: data.latitude.toString().substring(0, 8),
 					lon: data.longitude.toString().substring(0, 8),
