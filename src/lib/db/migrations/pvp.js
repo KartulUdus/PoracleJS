88cd1b305476872817ec6205a6a31b1de8d95b1c
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-07-28 18:31:02 +0200

    lint

--- src/lib/db/migrations/pvp.js
+++ src/lib/db/migrations/pvp.js
@@ -10,8 +10,8 @@ exports.up = async function migrationUp(knex) {
 		table.dropUnique(null, 'monsters_tracking')
 		table.unique([
 			'id', 'pokemon_id', 'min_iv', 'max_iv', 'min_level', 'max_level', 'atk', 'def', 'sta', 'form', 'gender', 'min_weight',
-			'great_league_ranking', 'great_league_ranking_min_cp', 'ultra_league_ranking', 'ultra_league_ranking_min_cp'
-		],'monsters_tracking')
+			'great_league_ranking', 'great_league_ranking_min_cp', 'ultra_league_ranking', 'ultra_league_ranking_min_cp',
+		], 'monsters_tracking')
 	})
 	log.info('PVP migration applied')
 }
