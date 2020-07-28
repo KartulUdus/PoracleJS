5ab83b539b0f435fbbe34672f6e00aa8a56ae305
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-07-28 18:30:38 +0200

    update monsters_tracking unique index removed: max_weight, max_atk, max_def, max_sta added: great_league_ranking, great_league_ranking_min_cp, ultra_league_ranking, ultra_league_ranking_min_cp

--- src/lib/db/migrations/pvp.js
+++ src/lib/db/migrations/pvp.js
@@ -6,6 +6,12 @@ exports.up = async function migrationUp(knex) {
 		table.integer('great_league_ranking_min_cp').notNullable().defaultTo(0)
 		table.integer('ultra_league_ranking').notNullable().defaultTo(4096)
 		table.integer('ultra_league_ranking_min_cp').notNullable().defaultTo(0)
+		// removed: max_weight, max_atk, max_def, max_sta; added: ^ 4 cols above
+		table.dropUnique(null, 'monsters_tracking')
+		table.unique([
+			'id', 'pokemon_id', 'min_iv', 'max_iv', 'min_level', 'max_level', 'atk', 'def', 'sta', 'form', 'gender', 'min_weight',
+			'great_league_ranking', 'great_league_ranking_min_cp', 'ultra_league_ranking', 'ultra_league_ranking_min_cp'
+		],'monsters_tracking')
 	})
 	log.info('PVP migration applied')
 }
