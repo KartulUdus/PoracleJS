31434cfd85ca4dbf06277ada3385ab9888208b06
James Berry <james@coppermoth.com>
2020-10-29 21:01:24 +0000

    Change !poracle to always message user

--- src/lib/discord/commando/commands/poracle.js
+++ src/lib/discord/commando/commands/poracle.js
@@ -5,11 +5,14 @@ exports.run = async (client, msg) => {
 	}
 	try {
 		const isRegistered = await client.query.countQuery('humans', { id: msg.author.id })
-		if (isRegistered) return await msg.react('👌')
+		if (isRegistered) {
 
-		await client.query.insertQuery('humans', {
-			id: msg.author.id, type: 'discord:user', name: client.emojiStrip(msg.author.username), area: '[]',
-		})
+			await msg.react('👌')
+		} else {
+			await client.query.insertQuery('humans', {
+				id: msg.author.id, type: 'discord:user', name: client.emojiStrip(msg.author.username), area: '[]',
+			})
+		}
 		await msg.react('✅')
 		const greetingDts = client.dts.find((template) => template.type === 'greeting')
 		const view = { prefix: client.config.discord.prefix }
