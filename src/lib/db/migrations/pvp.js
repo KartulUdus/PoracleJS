77fb1ce32c354adbf74a206df94b4e11fc76c669
James Berry <james@coppermoth.com>
2020-12-05 17:10:45 +0000

    sqlite3 fix for pvp migration

--- src/lib/db/migrations/pvp.js
+++ src/lib/db/migrations/pvp.js
@@ -9,14 +9,14 @@ exports.up = async function migrationUp(knex) {
 		table.integer('great_league_ranking_min_cp').notNullable().defaultTo(0)
 		table.integer('ultra_league_ranking').notNullable().defaultTo(4096)
 		table.integer('ultra_league_ranking_min_cp').notNullable().defaultTo(0)
-		if (config.database.client !== 'sqlite')
+		if (config.database.client !== 'sqlite' && config.database.client !== 'sqlite3')
 			table.dropForeign(['id'])
 		table.dropUnique(null, 'monsters_tracking')
 		table.unique([
 			'id', 'pokemon_id', 'min_iv', 'max_iv', 'min_level', 'max_level', 'atk', 'def', 'sta', 'form', 'gender', 'min_weight',
 			'great_league_ranking', 'great_league_ranking_min_cp', 'ultra_league_ranking', 'ultra_league_ranking_min_cp',
 		], 'monsters_tracking')
-		if (config.database.client !== 'sqlite')
+		if (config.database.client !== 'sqlite' && config.database.client !== 'sqlite3')
 			table.foreign('id').references('humans.id').onDelete('CASCADE')
 	})
 	log.info('PVP migration applied')
