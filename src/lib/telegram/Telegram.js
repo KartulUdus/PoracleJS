1074ca2e5de9da3eacdfc4e0129dffeb94873461
James Berry <james@coppermoth.com>
2021-01-04 21:17:24 +0000

    Add control of webpage_preview + error checking for photos

--- src/lib/telegram/Telegram.js
+++ src/lib/telegram/Telegram.js
@@ -37,7 +37,9 @@ class Telegram {
 		this.init()
 	}
 
-	static sleep(n) { return new Promise((resolve) => setTimeout(resolve, n)) }
+	static sleep(n) {
+		return new Promise((resolve) => setTimeout(resolve, n))
+	}
 
 	init() {
 		this.bot.launch()
@@ -72,22 +74,25 @@ class Telegram {
 		this.log.warn(`Telegram sending message ${JSON.stringify(data.message)} / connection to ${data.name} ${data.target}`)
 		try {
 			try {
-				if (data.message.sticker) {
+				if (data.message.sticker && data.message.sticker.length > 0) {
 					await this.bot.telegram.sendSticker(data.target, data.message.sticker, {disable_notification: true})
 				}
+			} catch (err) {
+				this.log.error(`Failed to send Telegram sticker ${data.message.sticker} to ${data.name}/${data.target}, ${err.message}`)
 			}
-			catch (err) {
-				this.log.error(`Failed to send Telegram sticker to ${data.name}/${data.target}, ${err.message}`)
-			}
-			if (data.message.photo) {
-				await this.bot.telegram.sendPhoto(data.target, data.message.photo, { disable_notification: true })
+			try {
+				if (data.message.photo && data.message.photo.length > 0) {
+					await this.bot.telegram.sendPhoto(data.target, data.message.photo, {disable_notification: true})
+				}
+			} catch (err) {
+				this.log.error(`Failed to send Telegram photo ${data.message.photo} to ${data.name}/${data.target}, ${err.message}`)
 			}
 			await this.bot.telegram.sendMessage(data.target, data.message.content || data.message || '', {
 				parse_mode: 'Markdown',
-				disable_web_page_preview: true,
+				disable_web_page_preview: !data.message.webpage_preview,
 			})
 			if (data.message.location) {
-				await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, { disable_notification: true })
+				await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, {disable_notification: true})
 			}
 		} catch (err) {
 			this.log.error(`Failed to send Telegram alert to ${data.name}/${data.target}, ${err.message}`)
