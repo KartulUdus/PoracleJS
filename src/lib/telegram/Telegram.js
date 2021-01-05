dc24a1e74de8b90661dec7870b6cef7b25701a83
James Berry <james@coppermoth.com>
2021-01-05 12:33:49 +0000

    Implemented support for clean for telegram

--- src/lib/telegram/Telegram.js
+++ src/lib/telegram/Telegram.js
@@ -73,26 +73,41 @@ class Telegram {
 	async userAlert(data) {
 		this.log.warn(`Telegram sending message ${JSON.stringify(data.message)} / connection to ${data.name} ${data.target}`)
 		try {
+			const msgDeletionMs = ((data.tth.hours * 3600) + (data.tth.minutes * 60) + data.tth.seconds) * 1000
+			const messageIds = []
 			try {
 				if (data.message.sticker && data.message.sticker.length > 0) {
-					await this.bot.telegram.sendSticker(data.target, data.message.sticker, { disable_notification: true })
+					const { message_id } = await this.bot.telegram.sendSticker(data.target, data.message.sticker, { disable_notification: true })
+					messageIds.push(message_id)
 				}
 			} catch (err) {
 				this.log.error(`Failed to send Telegram sticker ${data.message.sticker} to ${data.name}/${data.target}, ${err.message}`)
 			}
 			try {
 				if (data.message.photo && data.message.photo.length > 0) {
-					await this.bot.telegram.sendPhoto(data.target, data.message.photo, { disable_notification: true })
+					const { message_id } = await this.bot.telegram.sendPhoto(data.target, data.message.photo, { disable_notification: true })
+					messageIds.push(message_id)
 				}
 			} catch (err) {
 				this.log.error(`Failed to send Telegram photo ${data.message.photo} to ${data.name}/${data.target}, ${err.message}`)
 			}
-			await this.bot.telegram.sendMessage(data.target, data.message.content || data.message || '', {
+			const { message_id } = await this.bot.telegram.sendMessage(data.target, data.message.content || data.message || '', {
 				parse_mode: 'Markdown',
 				disable_web_page_preview: !data.message.webpage_preview,
 			})
+			messageIds.push(message_id)
+
 			if (data.message.location) {
-				await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, { disable_notification: true })
+				const { message_id } =  await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, { disable_notification: true })
+				messageIds.push(message_id)
+			}
+
+			if (data.clean) {
+				setTimeout(() => {
+					for (const id of messageIds) {
+						this.bot.telegram.deleteMessage(data.target, id)
+					}
+				}, msgDeletionMs)
 			}
 		} catch (err) {
 			this.log.error(`Failed to send Telegram alert to ${data.name}/${data.target}, ${err.message}`)
