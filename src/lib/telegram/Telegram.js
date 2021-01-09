d68a749a41099fac4a5d7e9c65de1ebd08f87b70
James Berry <james@coppermoth.com>
2021-01-09 09:47:01 +0000

    Fix clean bug introduced

--- src/lib/telegram/Telegram.js
+++ src/lib/telegram/Telegram.js
@@ -84,30 +84,30 @@ class Telegram {
 			const messageIds = []
 			try {
 				if (data.message.sticker && data.message.sticker.length > 0) {
-					const { messageId } = await this.bot.telegram.sendSticker(data.target, data.message.sticker, { disable_notification: true })
-					messageIds.push(messageId)
+					const msg = await this.bot.telegram.sendSticker(data.target, data.message.sticker, { disable_notification: true })
+					messageIds.push(msg.message_id)
 				}
 			} catch (err) {
 				this.log.error(`Failed to send Telegram sticker ${data.message.sticker} to ${data.name}/${data.target}, ${err.message}`)
 			}
 			try {
 				if (data.message.photo && data.message.photo.length > 0) {
-					const { messageId } = await this.bot.telegram.sendPhoto(data.target, data.message.photo, { disable_notification: true })
-					messageIds.push(messageId)
+					const msg = await this.bot.telegram.sendPhoto(data.target, data.message.photo, { disable_notification: true })
+					messageIds.push(msg.message_id)
 				}
 			} catch (err) {
 				this.log.error(`Failed to send Telegram photo ${data.message.photo} to ${data.name}/${data.target}, ${err.message}`)
 			}
-			const { messageId } = await this.bot.telegram.sendMessage(data.target, data.message.content || data.message || '', {
+			const msg = await this.bot.telegram.sendMessage(data.target, data.message.content || data.message || '', {
 				parse_mode: 'Markdown',
 				disable_web_page_preview: !data.message.webpage_preview,
 			})
-			messageIds.push(messageId)
+			messageIds.push(msg.message_id)
 
 			if (data.message.location) {
 				// eslint-disable-next-line no-shadow
-				const { messageId } = await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, { disable_notification: true })
-				messageIds.push(messageId)
+				const msg = await this.bot.telegram.sendLocation(data.target, data.lat, data.lon, { disable_notification: true })
+				messageIds.push(msg.message_id)
 			}
 
 			if (data.clean) {
