b3ebcff4e2f6a750e9a923e14a7d11d1b52b7dc7
James Berry <james@coppermoth.com>
2021-02-09 11:57:47 +0000

    Raid increase logging around mustache results

--- src/controllers/raid.js
+++ src/controllers/raid.js
@@ -310,7 +310,22 @@ class Raid extends Controller {
 
 					const mustache = this.getDts(logReference, 'raid', platform, cares.template, language)
 					if (mustache) {
-						const message = JSON.parse(mustache(view, { data: { language } }))
+						let mustacheResult
+						let message
+						try {
+							mustacheResult = mustache(view, { data: { language } })
+						} catch (err) {
+							this.log.error(`${logReference}: Error generating mustache results for ${platform}/${cares.template}/${language}`, err, view)
+							// eslint-disable-next-line no-continue
+							continue
+						}
+						try {
+							message = JSON.parse(mustacheResult)
+						} catch (err) {
+							this.log.error(`${logReference}: Error JSON parsing mustache results ${mustacheResult}`, err)
+							// eslint-disable-next-line no-continue
+							continue
+						}
 
 						if (cares.ping) {
 							if (!message.content) {
@@ -402,7 +417,22 @@ class Raid extends Controller {
 
 				const mustache = this.getDts(logReference, 'egg', platform, cares.template, language)
 				if (mustache) {
-					const message = JSON.parse(mustache(view, { data: { language } }))
+					let mustacheResult
+					let message
+					try {
+						mustacheResult = mustache(view, { data: { language } })
+					} catch (err) {
+						this.log.error(`${logReference}: Error generating mustache results for ${platform}/${cares.template}/${language}`, err, view)
+						// eslint-disable-next-line no-continue
+						continue
+					}
+					try {
+						message = JSON.parse(mustacheResult)
+					} catch (err) {
+						this.log.error(`${logReference}: Error JSON parsing mustache results ${mustacheResult}`, err)
+						// eslint-disable-next-line no-continue
+						continue
+					}
 
 					if (cares.ping) {
 						if (!message.content) {
