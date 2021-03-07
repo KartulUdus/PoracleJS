0d38ef8654d00902d0914d7fe0b958b437843377
James Berry <jfberry@gmail.com>
2021-03-07 15:14:11 +0000

    Quality of life #1 (#378)
    Quality of life improvements 
    Co-authored-by: James Berry <james@coppermoth.com>

--- src/lib/discord/commando/events/message.js
+++ src/lib/discord/commando/events/message.js
@@ -15,43 +15,56 @@ module.exports = async (client, msg) => {
 			if (client.config.discord.admins.includes(msg.author.id)) {
 				message = `**${msg.author.username}** > ${msg.cleanContent}`
 			}
-			try {
-				const channel = await client.channels.fetch(client.config.discord.dmLogChannelID)
-				const msgDeletionMs = (client.config.discord.dmLogChannelDeletionTime * 60) * 1000 || 0
-				if (!channel) {
-					client.logs.discord.warn('channel dmLogChannel not found')
-				} else {
-					const logmsg = await channel.send(message)
-					if (msgDeletionMs > 0) {
-						logmsg.delete({ timeout: msgDeletionMs, reason: 'Removing old stuff.' })
+
+			client.channels.fetch(client.config.discord.dmLogChannelID).then(
+				(channel) => {
+					const msgDeletionMs = (client.config.discord.dmLogChannelDeletionTime * 60) * 1000 || 0
+					if (!channel) {
+						client.logs.discord.warn('channel dmLogChannel not found')
+					} else {
+						channel.send(message).then((logmsg) => {
+							if (msgDeletionMs > 0) {
+								logmsg.delete({ timeout: msgDeletionMs, reason: 'Removing old stuff.' })
+							}
+						}).catch(() => { })
 					}
-				}
-			} catch (err) {
+				},
+			).catch((err) => {
 				client.logs.discord.error('Failed to send Discord alert to dmLogChannel', err)
+			})
+		}
+
+		let recognisedCommand = false
+
+		for (const commandText of msg.content.split('\n')) {
+			// Ignore msgs not starting with the prefix (in config)
+			// eslint-disable-next-line no-continue
+			if (commandText.indexOf(client.config.discord.prefix) !== 0) continue
+
+			let args = commandText.slice(client.config.discord.prefix.length).trim().split(/ +/g)
+
+			args = args.map((arg) => client.translatorFactory.reverseTranslateCommand(arg.toLowerCase().replace(/_/g, ' '), true).toLowerCase())
+			const command = args.shift()
+
+			// eslint-disable-next-line no-continue
+			if (client.config.general.disabledCommands.includes(command)) continue
+
+			let initialArgs
+			if (args.includes('|')) {
+				initialArgs = args.join(' ').split('|').map((com) => com.split(' ').filter((a) => a))
+			} else {
+				initialArgs = [args]
+			}
+
+			const cmd = client.commands[command]
+			if (cmd) {
+				recognisedCommand = true
+				cmd.run(client, msg, initialArgs)
 			}
 		}
-
-		// Ignore msgs not starting with the prefix (in config)
-		if (msg.content.indexOf(client.config.discord.prefix) !== 0) return
-
-		let args = msg.content.slice(client.config.discord.prefix.length).trim().split(/ +/g)
-
-		args = args.map((arg) => client.translatorFactory.reverseTranslateCommand(arg.toLowerCase().replace(/_/g, ' '), true).toLowerCase())
-		const command = args.shift()
-
-		if (client.config.general.disabledCommands.includes(command)) return
-
-		let initialArgs
-		if (args.includes('|')) {
-			initialArgs = args.join(' ').split('|').map((com) => com.split(' ').filter((a) => a))
-		} else {
-			initialArgs = [args]
+		if (msg.channel.type === 'dm' && !recognisedCommand && client.config.discord.unrecognisedCommandMessage) {
+			msg.reply(client.config.discord.unrecognisedCommandMessage)
 		}
-
-		const cmd = client.commands[command]
-		if (!cmd) return
-
-		cmd.run(client, msg, initialArgs)
 	} catch (err) {
 		client.logs.discord.error('Error during message event', err)
 	}
