08c2ee089f9ab49877fe3121363e6bf3d592552b
James Berry <james@coppermoth.com>
2021-04-05 07:40:18 +0100

    Fix version support for telegram

--- src/lib/discord/commando/events/ready.js
+++ src/lib/discord/commando/events/ready.js
@@ -1,8 +1,4 @@
 module.exports = (client) => {
 	client.logs.discord.info(`Commando "${client.user.tag}" awaiting for orders!`)
-	client.user.setPresence({
-		game: {
-			name: 'PoracleJS',
-		},
-	})
+	client.user.setActivity('PoracleJS')
 }
