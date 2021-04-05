e0867363bb9236a545fd2163308f0f5a6fa51468
James Berry <james@coppermoth.com>
2021-04-05 07:47:31 +0100

    Make activity text configurable

--- src/lib/discord/commando/events/ready.js
+++ src/lib/discord/commando/events/ready.js
@@ -1,4 +1,6 @@
 module.exports = (client) => {
 	client.logs.discord.info(`Commando "${client.user.tag}" awaiting for orders!`)
-	client.user.setActivity('PoracleJS')
+	if (client.config.discord.activity) {
+		client.user.setActivity(client.config.discord.activity)
+	}
 }
