679cd8ef7cf73309c1a093666bd413a58d8c94c4
James Berry <james@coppermoth.com>
2021-04-05 08:14:50 +0100

    ...but leave some trace of Poracleness unless turned off

--- src/lib/discord/commando/events/ready.js
+++ src/lib/discord/commando/events/ready.js
@@ -1,6 +1,8 @@
 module.exports = (client) => {
-	client.logs.discord.info(`Commando "${client.user.tag}" awaiting for orders!`)
+	client.logs.discord.info(`Commando "${client.user.tag}" awaiting your orders!`)
 	if (client.config.discord.activity) {
-		client.user.setActivity(client.config.discord.activity)
+		const activity = client.config.discord.activity == 'PoracleJS' ? 'PoracleJS' : `${client.config.discord.activity} (Poracle)`
+
+		client.user.setActivity(activity)
 	}
 }
