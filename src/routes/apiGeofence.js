095b4262f9d6b686c8a074422494477b572407b6
James Berry <james@coppermoth.com>
2021-05-01 17:19:51 +0100

    Api for single area retrieval

--- src/routes/apiGeofence.js
+++ src/routes/apiGeofence.js
@@ -1,7 +1,7 @@
 const geofenceTileGenerator = require('../lib/geofenceTileGenerator')
 
 module.exports = async (fastify, options, next) => {
-	fastify.get('/api/geofence/all', options, async (req) => {
+	fastify.get('/api/geofence/:area/map', options, async (req) => {
 		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
 
 		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
@@ -12,18 +12,17 @@ module.exports = async (fastify, options, next) => {
 			return { status: 'authError', reason: 'incorrect or missing api secret' }
 		}
 
-		const promises = fastify.geofence.map((x) => geofenceTileGenerator.generateGeofenceTile(fastify.geofence, fastify.query.tileserverPregen, x.name).catch((e) => e))
-
-		const resultUrls = await Promise.all(promises)
-
-		const results = {}
-		for (let i = 0; i < fastify.geofence.length; i++) {
-			results[fastify.geofence[i].name] = resultUrls[i]
-		}
-
-		return {
-			status: 'ok',
-			areas: results,
+		try {
+			const url = await geofenceTileGenerator.generateGeofenceTile(fastify.geofence, fastify.query.tileserverPregen, req.params.area)
+			return {
+				status: 'ok',
+				url,
+			}
+		} catch (err) {
+			fastify.logger.error(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`, err)
+			return {
+				status: 'error',
+			}
 		}
 	})
 
