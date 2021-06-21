3c8badac39599a91df9f23e21555a3db0d28ffc7
James Berry <james@coppermoth.com>
2021-06-21 10:30:24 +0100

    Convert to new object comparison library

--- src/lib/poracleMessage/commands/gym.js
+++ src/lib/poracleMessage/commands/gym.js
@@ -1,6 +1,5 @@
 const helpCommand = require('./help.js')
 const trackedCommand = require('./tracked.js')
-const objectDiff = require('../../objectDiff')
 
 exports.run = async (client, msg, args, options) => {
 	const logReference = Math.random().toString().slice(2, 11)
@@ -91,8 +90,8 @@ exports.run = async (client, msg, args, options) => {
 			for (let i = insert.length - 1; i >= 0; i--) {
 				const toInsert = insert[i]
 
-				for (const existing of tracked.filter((x) => x.team_id == toInsert.team_id)) {
-					const differences = objectDiff.diff(existing, toInsert)
+				for (const existing of tracked.filter((x) => x.team_id === toInsert.team_id)) {
+					const differences = client.updatedDiff(existing, toInsert)
 
 					switch (Object.keys(differences).length) {
 						case 1:		// No differences (only UID)
@@ -131,11 +130,15 @@ exports.run = async (client, msg, args, options) => {
 				})
 			}
 
+			await client.query.deleteWhereInQuery('lures', {
+				id: target.id,
+				profile_no: currentProfileNo,
+			},
+			updates.map((x) => x.uid),
+			'uid')
+
 			if (insert.length) {
 				await client.query.insertQuery('gym', insert)
-			}
-			for (const row of updates) {
-				await client.query.updateQuery('gym', row, { uid: row.uid })
 			}
 			client.log.info(`${logReference}: ${target.name} started tracking gyms ${teams.join(', ')}`)
 			await msg.reply(message)
@@ -162,7 +165,7 @@ exports.run = async (client, msg, args, options) => {
 
 			msg.reply(
 				''.concat(
-					result == 1 ? translator.translate('I removed 1 entry')
+					result === 1 ? translator.translate('I removed 1 entry')
 						: translator.translateFormat('I removed {0} entries', result),
 					', ',
 					translator.translateFormat('use `{0}{1}` to see what you are currently tracking', util.prefix, translator.translate('tracked')),
