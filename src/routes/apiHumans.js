e0881d215ed777170b553a0aa142103a6e3f065f
James Berry <james@coppermoth.com>
2021-06-28 14:29:17 +0100

    Update API details to include userSelectable

--- src/routes/apiHumans.js
+++ src/routes/apiHumans.js
@@ -31,7 +31,12 @@ module.exports = async (fastify, options, next) => {
 
 		return {
 			status: 'ok',
-			areas: fastify.geofence.filter((x) => allowedAreas.includes(x.name.toLowerCase())).filter((area) => (area.userSelectable === undefined || area.userSelectable)).map((x) => ({ name: x.name, group: x.group || '', description: x.description || '' })),
+			areas: fastify.geofence.filter((x) => allowedAreas.includes(x.name.toLowerCase())).map((x) => ({
+				name: x.name,
+				group: x.group || '',
+				description: x.description || '',
+				userSelectable: (x.userSelectable === undefined || x.userSelectable),
+			})),
 		}
 	})
 
