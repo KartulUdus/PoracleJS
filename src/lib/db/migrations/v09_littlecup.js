acdb46903680f2ae0a2fc81227e39fcd6f8c3254
James Berry <james@coppermoth.com>
2021-07-08 07:21:20 +0100

    Fix migration defaults

--- src/lib/db/migrations/v09_littlecup.js
+++ src/lib/db/migrations/v09_littlecup.js
@@ -3,7 +3,7 @@ const { log } = require('../../logger')
 exports.up = async function migrationUp(knex) {
 	await knex.schema.alterTable('monsters', (table) => {
 		table.integer('little_league_ranking').notNullable().defaultTo(4096)
-		table.integer('little_league_ranking_min_cp').notNullable().defaultTo(4096)
+		table.integer('little_league_ranking_min_cp').notNullable().defaultTo(0)
 		table.integer('little_league_ranking_highest').notNullable().defaultTo(1)
 		table.integer('great_league_ranking_highest').notNullable().defaultTo(1)
 		table.integer('ultra_league_ranking_highest').notNullable().defaultTo(1)
