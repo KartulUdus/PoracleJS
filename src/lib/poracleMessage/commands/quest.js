b1f90b30b4c017ddc2ca0ed6e6624158aec15b41
James Berry <james@coppermoth.com>
2021-08-24 20:47:39 +0100

    Always allow removal, regardless of command security

--- src/lib/poracleMessage/commands/quest.js
+++ src/lib/poracleMessage/commands/quest.js
@@ -22,7 +22,7 @@ exports.run = async (client, msg, args, options) => {
 
 		const translator = client.translatorFactory.Translator(language)
 
-		if (!await util.commandAllowed(commandName)) {
+		if (!await util.commandAllowed(commandName) && !args.find((arg) => arg === 'remove')) {
 			await msg.react('🚫')
 			return msg.reply(translator.translate('You do not have permission to execute this command'))
 		}
