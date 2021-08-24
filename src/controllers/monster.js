396932534527e35b1493ad44cb3e481ed0064a7b
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2021-08-24 15:13:00 -0400

    Non-Invasive Chuck Support (#521)
    * Non-Invasive Chuck Support
    
    - Adds the bare minimum Chuck PVP support
    - Non-invasive
    - Tracks level caps in DTS
    
    * Remove Unnecessary Handlebar Helper

--- src/controllers/monster.js
+++ src/controllers/monster.js
@@ -312,6 +312,7 @@ class Monster extends Controller {
 								form: stats.form || 0,
 								level: stats.level,
 								cp: stats.cp,
+								cap: stats.cap,
 							}
 							data.pvpEvolutionData[stats.pokemon] = {
 								...data.pvpEvolutionData[stats.pokemon],
@@ -328,6 +329,13 @@ class Monster extends Controller {
 				return {
 					rank: bestRank,
 					cp: bestCP,
+				}
+			}
+
+			if (data.pvp) {
+				// For Chuck parsers
+				for (const league of Object.keys(data.pvp)) {
+					data[`pvp_rankings_${league}_league`] = data.pvp[league]
 				}
 			}
 
