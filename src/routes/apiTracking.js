ca6aedf211ef9a6ab550041f065c0cc6b965f509
James Berry <james@coppermoth.com>
2021-08-27 21:53:18 +0100

    Add /api/tracking/all

--- src/routes/apiTracking.js
+++ src/routes/apiTracking.js
@@ -5,6 +5,52 @@ const { diff } = require('deep-object-diff')
 const trackedCommand = require('../lib/poracleMessage/commands/tracked')
 
 module.exports = async (fastify, options, next) => {
+	fastify.get('/api/tracking/all/:id', options, async (req) => {
+		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
+
+		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
+		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
+
+		const secret = req.headers['x-poracle-secret']
+		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
+			return { status: 'authError', reason: 'incorrect or missing api secret' }
+		}
+		const human = await fastify.query.selectOneQuery('humans', { id: req.params.id })
+
+		if (!human) {
+			return {
+				status: 'error',
+				message: 'User not found',
+			}
+		}
+		const currentProfileNo = human.current_profile_no
+		const { id } = req.params
+
+		const monsters = await fastify.query.selectAllQuery('monsters', { id, profile_no: currentProfileNo })
+		const raids = await fastify.query.selectAllQuery('raid', { id, profile_no: currentProfileNo })
+		const eggs = await fastify.query.selectAllQuery('egg', { id, profile_no: currentProfileNo })
+		const quests = await fastify.query.selectAllQuery('quest', { id, profile_no: currentProfileNo })
+		const invasions = await fastify.query.selectAllQuery('invasion', { id, profile_no: currentProfileNo })
+		const lures = await fastify.query.selectAllQuery('lures', { id, profile_no: currentProfileNo })
+		const nests = await fastify.query.selectAllQuery('nests', { id, profile_no: currentProfileNo })
+		const gyms = await fastify.query.selectAllQuery('gym', { id, profile_no: currentProfileNo })
+		const profile = await fastify.query.selectOneQuery('profiles', { id, profile_no: currentProfileNo })
+
+		return {
+			status: 'ok',
+			human,
+			gyms,
+			raids,
+			eggs,
+			monsters,
+			invasions,
+			lures,
+			nests,
+			quests,
+			profile,
+		}
+	})
+
 	fastify.get('/api/tracking/gym/:id', options, async (req) => {
 		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
 
