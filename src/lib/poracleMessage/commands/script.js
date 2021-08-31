23e91c6f2ebc01b749a4c95db0c322009dfd257c
James Berry <james@coppermoth.com>
2021-08-31 21:18:23 +0100

    More improvements

--- src/lib/poracleMessage/commands/script.js
+++ src/lib/poracleMessage/commands/script.js
@@ -40,50 +40,208 @@ exports.run = async (client, msg, args, options) => {
 		let message = ''
 		// allow user to override prefix
 		const { prefix } = util
+		const everything = true
 
 		if (human.latitude) message += `${prefix}location ${human.latitude},${human.longitude}\n`
 		const areas = JSON.parse(human.area)
 		if (areas.length) message += `${prefix}area add ${areas.map((x) => x.replace(/ /g, '_')).join(' ')}\n`
-		const monsterParameters = {
-			min: ['min_iv', -1],
-			maxiv: ['max_iv', 100],
-			mincp: ['min_cp', 0],
-			macxp: ['max_cp', 9000],
-			level: ['min_level', 0],
-			maxlevel: ['max_level', 40],
-			atk: ['atk', 0],
-			def: ['def', 0],
-			sta: ['sta', 0],
-			maxatk: ['max_atk', 0],
-			maxsta: ['max_sta', 0],
-			maxdef: ['max_def', 0],
-			weight: ['min_weight', 0],
-			maxweight: ['max_weight', 9000000],
-			rarity: ['rarity', -1],
-			maxrarity: ['max_rarity', 6],
-			t: ['min_time', 0],
-			template: ['template', client.config.general.defaultTemplateName.toString()],
-			d: ['distance', 0],
-		}
-		for (const monster of monsters) {
-			//			const mon = client.GameData.monsters.find()
-			message += `${prefix}track ${monster.pokemon_id ? monster.pokemon_id : 'everything'}`
-			if (monster.form) message += ` form:${monster.form}` // will not work
 
-			for (const [param, [dbFieldName, defaultValue]] of Object.entries(monsterParameters)) {
-				if (monster[dbFieldName] !== defaultValue) message += ` ${param}:${monster[dbFieldName]}`
+		const gender = ['', 'male', 'female', 'genderless']
+
+		if (everything || args.includes('monsters')) {
+			const monsterParameters = {
+				iv: ['min_iv', -1],
+				maxiv: ['max_iv', 100],
+				mincp: ['min_cp', 0],
+				macxp: ['max_cp', 9000],
+				level: ['min_level', 0],
+				maxlevel: ['max_level', 40],
+				atk: ['atk', 0],
+				def: ['def', 0],
+				sta: ['sta', 0],
+				maxatk: ['max_atk', 15],
+				maxsta: ['max_sta', 15],
+				maxdef: ['max_def', 15],
+				weight: ['min_weight', 0],
+				maxweight: ['max_weight', 9000000],
+				rarity: ['rarity', -1],
+				maxrarity: ['max_rarity', 6],
+				t: ['min_time', 0],
+				template: ['template', client.config.general.defaultTemplateName.toString()],
+				d: ['distance', 0],
+			}
+			for (const monster of monsters) {
+				const mon = client.GameData.monsters[`${monster.pokemon_id}_${monster.form}`]
+
+				message += `${prefix}track ${monster.pokemon_id ? mon.name.replace(/ /g, '_') : 'everything'}`
+				if (monster.form) message += ` form:${mon.form.name.replace(/ /g, '_')}`
+
+				for (const [param, [dbFieldName, defaultValue]] of Object.entries(monsterParameters)) {
+					if (monster[dbFieldName] !== defaultValue) message += ` ${param}:${monster[dbFieldName]}`
+				}
+
+				if (monster.clean) message += ' clean'
+				if (monster.gender) message += ` ${gender[monster.gender]}`
+
+				if (monster.pvp_ranking_league) {
+					const leagues = {
+						500: 'little',
+						1500: 'great',
+						2500: 'ultra',
+					}
+					const leagueName = leagues[monster.pvp_ranking_league]
+					message += ` ${leagueName}:${monster.pvp_ranking_worst} ${leagueName}cp:${monster.pvp_ranking_min_cp}`
+					if (monster.pvp_ranking_worst > 1) message += ` ${leagueName}high:${monster.pvp_ranking_best}`
+				}
+				// league
+
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('raid')) {
+			const raidParameters = {
+				template: ['template', client.config.general.defaultTemplateName.toString()],
+				d: ['distance', 0],
+			}
+			const raidTeams = ['harmony', 'mystic', 'valour', 'instinct']
+
+			for (const raid of raids) {
+				message += `${prefix}raid `
+				if (raid.pokemon_id === 9000) {
+					message += `level:${raid.level}`
+				} else {
+					const mon = client.GameData.monsters[`${raid.pokemon_id}_${raid.form}`]
+
+					message += `${mon.name}`
+					if (raid.form) message += ` form:${mon.form.name}` // will not work
+				}
+				for (const [param, [dbFieldName, defaultValue]] of Object.entries(raidParameters)) {
+					if (raid[dbFieldName] !== defaultValue) message += ` ${param}:${raid[dbFieldName]}`
+				}
+				if (raid.team !== 4) message += ` team:${raidTeams[raid.team]}`
+				if (raid.exclusive) message += ' ex'
+				if (raid.clean) message += ' clean'
+
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('eggs')) {
+			const eggParameters = {
+				template: ['template', client.config.general.defaultTemplateName.toString()],
+				d: ['distance', 0],
+			}
+			const raidTeams = ['harmony', 'mystic', 'valour', 'instinct']
+
+			for (const egg of eggs) {
+				message += `${prefix}egg level:${egg.level}`
+
+				for (const [param, [dbFieldName, defaultValue]] of Object.entries(eggParameters)) {
+					if (egg[dbFieldName] !== defaultValue) message += ` ${param}:${egg[dbFieldName]}`
+				}
+				if (egg.team !== 4) message += ` team:${raidTeams[egg.team]}`
+				if (egg.exclusive) message += ' ex'
+				if (egg.clean) message += ' clean'
+
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('invasion')) {
+			const invasionParameters = {
+				template: ['template', client.config.general.defaultTemplateName.toString()],
+				d: ['distance', 0],
 			}
 
-			if (monster.clean) message += ' clean'
-			// gender
-			// league
-
-			message += '\n'
+			for (const invasion of invasions) {
+				message += `${prefix}invasion ${invasion.grunt_type.replace(/ /g, '_')}`
+				if (invasion.gender) message += ` ${gender[invasion.gender]}`
+				for (const [param, [dbFieldName, defaultValue]] of Object.entries(invasionParameters)) {
+					if (invasion[dbFieldName] !== defaultValue) message += ` ${param}:${invasion[dbFieldName]}`
+				}
+				if (invasion.clean) message += ' clean'
+			}
 		}
 
-		msg.reply(message)
+		if (everything || args.includes('quest')) {
+			const questParameters = {
+				template: ['template', client.config.general.defaultTemplateName.toString()],
+				d: ['distance', 0],
+			}
+			for (const quest of quests) {
+				message += `${prefix}quest`
+				switch (quest.reward_type) {
+					case 3: {
+						if (quest.reward) message += ` stardust:${quest.reward}`
+						else message += ' stardust'
+						break
+					}
+					case 12: {
+						if (quest.reward) {
+							const mon = client.GameData.monsters[`${quest.reward}_0`]
+							message += ` energy:${mon.name.replace(/ /g, '_')}`
+						} else message += ' energy'
+						break
+					}
+					case 4: {
+						if (quest.reward) {
+							const mon = client.GameData.monsters[`${quest.reward}_0`]
+							message += ` candy:${mon.name.replace(/ /g, '_')}`
+						} else message += ' candy'
+						break
+					}
+					case 7: {
+						const mon = client.GameData.monsters[`${quest.reward}_${quest.form}`]
+						message += ` ${mon.name.replace(/ /g, '_')}`
+						if (quest.form) {
+							message += ` form:${mon.form.name.replace(/ /g, '_')}`
+						}
+						break
+					}
+					case 2: {
+						message += ` ${client.GameData.items[quest.reward].name.replace(/ /g, '_')}`
+					}
+					default:
+						break
+				}
 
-		if (args.contain('link')) {
+				if (quest.shiny) message += ' shiny'
+				for (const [param, [dbFieldName, defaultValue]] of Object.entries(questParameters)) {
+					if (quest[dbFieldName] !== defaultValue) message += ` ${param}:${quest[dbFieldName]}`
+				}
+
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('gym')) {
+			for (const gym of gyms) {
+				message += `${prefix}gym`
+
+				if (gym.slot_changes) message += ' slot_changes'
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('lure')) {
+			for (const lure of lures) {
+				message += `${prefix}lure`
+
+				message += '\n'
+			}
+		}
+
+		if (everything || args.includes('nests')) {
+			for (const nest of nests) {
+				message += `${prefix}nest`
+
+				message += '\n'
+			}
+		}
+		// msg.reply(message)
+
+		if (args.includes('link')) {
 			try {
 				const hastelink = await client.hastebin(message)
 				return await msg.reply(`Your backup is at ${hastelink}`)
