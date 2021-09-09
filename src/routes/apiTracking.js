08800b2e82f95c3ee5253101f08ea165f3cbc7ca
James Berry <james@coppermoth.com>
2021-09-09 08:45:16 +0100

    Tidy

--- src/routes/apiTracking.js
+++ src/routes/apiTracking.js
@@ -26,27 +26,27 @@ module.exports = async (fastify, options, next) => {
 		const currentProfileNo = human.current_profile_no
 		const { id } = req.params
 
-		const monsters = await fastify.query.selectAllQuery('monsters', { id, profile_no: currentProfileNo })
-		const raids = await fastify.query.selectAllQuery('raid', { id, profile_no: currentProfileNo })
-		const eggs = await fastify.query.selectAllQuery('egg', { id, profile_no: currentProfileNo })
-		const quests = await fastify.query.selectAllQuery('quest', { id, profile_no: currentProfileNo })
-		const invasions = await fastify.query.selectAllQuery('invasion', { id, profile_no: currentProfileNo })
-		const lures = await fastify.query.selectAllQuery('lures', { id, profile_no: currentProfileNo })
-		const nests = await fastify.query.selectAllQuery('nests', { id, profile_no: currentProfileNo })
-		const gyms = await fastify.query.selectAllQuery('gym', { id, profile_no: currentProfileNo })
+		const pokemon = await fastify.query.selectAllQuery('monsters', { id, profile_no: currentProfileNo })
+		const raid = await fastify.query.selectAllQuery('raid', { id, profile_no: currentProfileNo })
+		const egg = await fastify.query.selectAllQuery('egg', { id, profile_no: currentProfileNo })
+		const quest = await fastify.query.selectAllQuery('quest', { id, profile_no: currentProfileNo })
+		const invasion = await fastify.query.selectAllQuery('invasion', { id, profile_no: currentProfileNo })
+		const lure = await fastify.query.selectAllQuery('lures', { id, profile_no: currentProfileNo })
+		const nest = await fastify.query.selectAllQuery('nests', { id, profile_no: currentProfileNo })
+		const gym = await fastify.query.selectAllQuery('gym', { id, profile_no: currentProfileNo })
 		const profile = await fastify.query.selectOneQuery('profiles', { id, profile_no: currentProfileNo })
 
 		return {
 			status: 'ok',
 			human,
-			gyms,
-			raids,
-			eggs,
-			monsters,
-			invasions,
-			lures,
-			nests,
-			quests,
+			gym,
+			raid,
+			egg,
+			pokemon,
+			invasion,
+			lure,
+			nest,
+			quest,
 			profile,
 		}
 	})
@@ -90,37 +90,6 @@ module.exports = async (fastify, options, next) => {
 		}
 
 		await fastify.query.deleteQuery('gym', { id: req.params.id, uid: req.params.uid })
-
-		return {
-			status: 'ok',
-		}
-	})
-
-	fastify.delete('/api/tracking/gym/:id/byGym/:gym_id', options, async (req) => {
-		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
-
-		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
-		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
-
-		const secret = req.headers['x-poracle-secret']
-		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
-			return { status: 'authError', reason: 'incorrect or missing api secret' }
-		}
-
-		const human = await fastify.query.selectOneQuery('humans', { id: req.params.id })
-
-		if (!human) {
-			return {
-				status: 'error',
-				message: 'User not found',
-			}
-		}
-
-		await fastify.query.deleteQuery('gym', {
-			id: req.params.id,
-			gym_id: req.params.gym_id,
-			profile_no: human.current_profile_no,
-		})
 
 		return {
 			status: 'ok',
