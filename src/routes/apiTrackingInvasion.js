3bbc3b2d0a1681a365acb2dbfcc454559ed2bf29
James Berry <james@coppermoth.com>
2021-10-15 10:01:26 +0100

    Add invasion, lure, nest, quest endpoints

---
+++ src/routes/apiTrackingInvasion.js
@@ -0,0 +1,190 @@
+const { diff } = require('deep-object-diff')
+
+const trackedCommand = require('../lib/poracleMessage/commands/tracked')
+
+module.exports = async (fastify, options, next) => {
+	fastify.get('/api/tracking/invasion/:id', options, async (req) => {
+		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
+
+		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
+		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
+
+		const secret = req.headers['x-poracle-secret']
+		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
+			return { status: 'authError', reason: 'incorrect or missing api secret' }
+		}
+		const human = await fastify.query.selectOneQuery('humans', { id: req.params.id })
+
+		if (!human) {
+			return {
+				status: 'error',
+				message: 'User not found',
+			}
+		}
+
+		const invasion = await fastify.query.selectAllQuery('invasion', { id: req.params.id, profile_no: human.current_profile_no })
+
+		return {
+			status: 'ok',
+			invasion,
+		}
+	})
+
+	fastify.delete('/api/tracking/invasion/:id/byUid/:uid', options, async (req) => {
+		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
+
+		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
+		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
+
+		const secret = req.headers['x-poracle-secret']
+		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
+			return { status: 'authError', reason: 'incorrect or missing api secret' }
+		}
+
+		await fastify.query.deleteQuery('invasion', { id: req.params.id, uid: req.params.uid })
+
+		return {
+			status: 'ok',
+		}
+	})
+
+	fastify.post('/api/tracking/invasion/:id', options, async (req) => {
+		fastify.logger.info(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`)
+
+		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
+		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
+
+		const secret = req.headers['x-poracle-secret']
+		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
+			return { status: 'authError', reason: 'incorrect or missing api secret' }
+		}
+
+		const human = await fastify.query.selectOneQuery('humans', { id: req.params.id })
+
+		if (!human) {
+			return {
+				status: 'error',
+				message: 'User not found',
+			}
+		}
+
+		const language = human.language || fastify.config.general.locale
+		const translator = fastify.translatorFactory.Translator(language)
+		const { id } = req.params
+		const currentProfileNo = human.current_profile_no
+
+		let insertReq = req.body
+		if (!Array.isArray(insertReq)) insertReq = [insertReq]
+
+		const defaultTo = ((value, x) => ((value === undefined) ? x : value))
+
+		const insert = insertReq.map((row) => {
+			if (!row.grunt_type) {
+				throw new Error('Grunt type mandatory')
+			}
+			return {
+				id,
+				profile_no: currentProfileNo,
+				ping: '',
+				template: (row.template || fastify.config.general.defaultTemplateName).toString(),
+				distance: +defaultTo(row.distance, 0),
+				clean: +defaultTo(row.clean, 0),
+				gender: +defaultTo(row.clean, 0),
+				grunt_type: row.grun_type,
+			}
+		})
+
+		try {
+			const tracked = await fastify.query.selectAllQuery('invasion', { id, profile_no: currentProfileNo })
+
+			const updates = []
+			const alreadyPresent = []
+
+			for (let i = insert.length - 1; i >= 0; i--) {
+				const toInsert = insert[i]
+
+				for (const existing of tracked.filter((x) => x.grunt_type === toInsert.grunt_type)) {
+					const differences = diff(existing, toInsert)
+
+					switch (Object.keys(differences).length) {
+						case 1:		// No differences (only UID)
+							// No need to insert
+							alreadyPresent.push(toInsert)
+							insert.splice(i, 1)
+							break
+						case 2:		// One difference (something + uid)
+							if (Object.keys(differences).some((x) => ['distance', 'template', 'clean'].includes(x))) {
+								updates.push({
+									...toInsert,
+									uid: existing.uid,
+								})
+								insert.splice(i, 1)
+							}
+							break
+						default:	// more differences
+							break
+					}
+				}
+			}
+
+			let message = ''
+
+			if ((alreadyPresent.length + updates.length + insert.length) > 50) {
+				message = translator.translateFormat('I have made a lot of changes. See {0}{1} for details', '!', /* util.prefix, */ translator.translate('tracked'))
+			} else {
+				for (const lure of alreadyPresent) {
+					message = message.concat(translator.translate('Unchanged: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				}
+				for (const lure of updates) {
+					message = message.concat(translator.translate('Updated: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				}
+				for (const lure of insert) {
+					message = message.concat(translator.translate('New: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				}
+			}
+
+			await fastify.query.deleteWhereInQuery('invasion', {
+				id,
+				profile_no: currentProfileNo,
+			},
+			updates.map((x) => x.uid),
+			'uid')
+
+			await fastify.query.insertQuery('invasion', [...insert, ...updates])
+
+			// Send message to user
+
+			const data = [{
+				lat: 0,
+				lon: 0,
+				message: { content: message },
+				target: human.id,
+				type: human.type,
+				name: human.name,
+				tth: { hours: 1, minutes: 0, seconds: 0 },
+				clean: false,
+				emoji: '',
+				logReference: 'WebApi',
+				language,
+			}]
+
+			data.forEach((job) => {
+				if (['discord:user', 'discord:channel', 'webhook'].includes(job.type)) fastify.discordQueue.push(job)
+				if (['telegram:user', 'telegram:channel'].includes(job.type)) fastify.telegramQueue.push(job)
+			})
+
+			return {
+				status: 'ok',
+				message,
+			}
+		} catch (err) {
+			fastify.logger.error(`API: ${req.ip} ${req.context.config.method} ${req.context.config.url}`, err)
+			return {
+				status: 'error',
+				message: 'Exception raised during execution',
+			}
+		}
+	})
+
+	next()
+}
