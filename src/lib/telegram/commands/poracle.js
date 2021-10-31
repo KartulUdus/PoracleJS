0c1ff672978d989ba08ecf198ac9aadde402049a
Petap0w <cks85@msn.com>
2021-10-31 17:23:35 +0100

    Escape markdowns fix

--- src/lib/telegram/commands/poracle.js
+++ src/lib/telegram/commands/poracle.js
@@ -102,7 +102,7 @@ module.exports = async (ctx) => {
 		}
 
 		if (client.config.telegram.groupWelcomeText) {
-			await ctx.reply(`${controller.config.telegram.groupWelcomeText.replace('{user}', userName.replace(/[_*[`]/g, ((m) => `\\${m}`)))}`, { parse_mode: 'Markdown' })
+			await ctx.reply(`${controller.config.telegram.groupWelcomeText.replace('{user}', userName.replace(/[_*[\]`]/g, ((m) => `\\${m}`)))}`, { parse_mode: 'Markdown' })
 		}
 
 		const dts = controller.dts.find((template) => template.type === 'greeting' && template.platform === 'telegram' && template.default)
