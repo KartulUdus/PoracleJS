b1cb8fc001430ba56d18c5e51b50aa98a5015b7b
James Berry <james@coppermoth.com>
2021-12-28 11:36:13 +0000

    Lint fix

--- src/lib/cache/cacheFactory.js
+++ src/lib/cache/cacheFactory.js
@@ -3,10 +3,15 @@ const PoracleNodeCache = require('./standardCache')
 
 function createCache(config, ttl) {
 	if (config.database.cache === 'redis') {
-		return new PoracleRedisCache(ttl, config.database.redis.host,
-			config.database.redis.port, config.database.redis.database,
-			config.database.redis.user, config.database.redis.password,
-			config.database.redis.prefix)
+		return new PoracleRedisCache(
+			ttl,
+			config.database.redis.host,
+			config.database.redis.port,
+			config.database.redis.database,
+			config.database.redis.user,
+			config.database.redis.password,
+			config.database.redis.prefix,
+		)
 	}
 
 	return new PoracleNodeCache(ttl)
