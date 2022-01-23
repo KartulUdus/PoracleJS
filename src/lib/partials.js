bf72224015806bd3dc1cf335ba9a2ac03b3b4511
James Berry <james@coppermoth.com>
2022-01-23 12:10:20 +0000

    Live through missing partials file

--- src/lib/partials.js
+++ src/lib/partials.js
@@ -3,9 +3,12 @@ const fs = require('fs')
 const path = require('path')
 
 function registerPartials(handlebars) {
+	const filename = path.join(__dirname, '../../config/partials.json')
+	if (!fs.existsSync(filename)) return
+
 	let partials
 	try {
-		const partialText = stripJsonComments(fs.readFileSync(path.join(__dirname, '../../config/partials.json'), 'utf8'))
+		const partialText = stripJsonComments(fs.readFileSync(filename, 'utf8'))
 		partials = JSON.parse(partialText)
 	} catch (err) {
 		throw new Error(`partials.json - ${err.message}`)
