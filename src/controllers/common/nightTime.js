1144efb68961f4ef07111f71cf4bbd62a9142708
James Berry <james@coppermoth.com>
2022-04-28 19:58:49 +0100

    Clean up

--- src/controllers/common/nightTime.js
+++ src/controllers/common/nightTime.js
@@ -1,30 +1,14 @@
 /* Night Time caclulations */
 const moment = require('moment-timezone')
-const {
-	getSunset,
-	getSunrise,
-} = require('sunrise-sunset-js')
 
 const SunCalc = require('suncalc')
 
-// function convertToTz(time, tz) {
-// 	const options = {timeZone: tz, year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'}
-// 	return new Date(Date.parse(new Intl.DateTimeFormat('default', options).format(time)))
-//
-// }
-
 /* Night time clculations */
 
-function setNightTime(data, checkTime, tz) {
-	// const sunsetTime = moment(getSunset(data.latitude, data.longitude, checkTime.toDate()))
-	// const sunriseTime = moment(getSunrise(data.latitude, data.longitude, checkTime.toDate()))
-
+function setNightTime(data, checkTime) {
 	const times = SunCalc.getTimes(checkTime.toDate(), data.latitude, data.longitude)
-
 	const sunsetTime = moment(times.sunset)
 	const sunriseTime = moment(times.sunrise)
-
-//	const sunsetTime3 = moment(convertToTz(times.sunset, tz))
 
 	const dawnEndTime = moment(sunriseTime).add({ hours: 1 })
 	const duskStartTime = moment(sunsetTime).subtract({ hours: 1 })
