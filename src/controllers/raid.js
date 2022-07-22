6e962916beeb3efe9c03c2e4033ac86d27f31b5c
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-22 17:43:02 +0200

    Update raid.js

--- src/controllers/raid.js
+++ src/controllers/raid.js
@@ -141,6 +141,8 @@ class Raid extends Controller {
 			data.googleMapUrl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`
 			data.appleMapUrl = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`
 			data.wazeMapUrl = `https://www.waze.com/ul?ll=${data.latitude},${data.longitude}&navigate=yes&zoom=17`
+			data.reactMapUrl = `${this.config.general.reactMapURL}/id/gyms/${data.gymId}`
+			data.rocketMadUrl = `${this.config.general.rocketMadURL}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 
 			data.team_id ??= 0
 			if (data.name) {
