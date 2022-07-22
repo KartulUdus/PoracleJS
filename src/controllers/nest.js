d73345c3813cb2c89da864bc2f4ae3760be0ba00
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-22 17:50:23 +0200

    Update nest.js

--- src/controllers/nest.js
+++ src/controllers/nest.js
@@ -78,6 +78,8 @@ class Nest extends Controller {
 			data.googleMapUrl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`
 			data.appleMapUrl = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`
 			data.wazeMapUrl = `https://www.waze.com/ul?ll=${data.latitude},${data.longitude}&navigate=yes&zoom=17`
+			data.reactMapUrl = `${this.config.general.reactMapURL}/id/nests/${data.nest_id}`
+			data.rocketMadUrl = `${this.config.general.rocketMadURL}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 			data.name = this.escapeJsonString(data.name)
 
 			const nestExpiration = data.reset_time + (7 * 24 * 60 * 60)
