60d8e40b0199d67f5f89c4a9dd21cee739827755
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-23 12:12:17 +0200

    Update quest.js

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -108,7 +108,7 @@ class Quest extends Controller {
 			data.googleMapUrl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`
 			data.appleMapUrl = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`
 			data.wazeMapUrl = `https://www.waze.com/ul?ll=${data.latitude},${data.longitude}&navigate=yes&zoom=17`
-			data.reactMapUrl = `${this.config.general.reactMapURL}/id/pokestops/${data.pokestop_id}`
+			data.reactMapUrl = `${this.config.general.reactMapURL}id/pokestops/${data.pokestop_id}`
 			data.rocketMadUrl = `${this.config.general.rocketMadURL}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 			data.disappearTime = moment.tz(new Date(), this.config.locale.time, geoTz.find(data.latitude, data.longitude)[0].toString()).endOf('day')
 			data.applemap = data.appleMapUrl // deprecated
