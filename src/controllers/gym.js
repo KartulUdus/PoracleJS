db38f9088c56a8175ed8f4ca5d6890f04cb2ad18
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-23 11:59:25 +0200

    Update gym.js

--- src/controllers/gym.js
+++ src/controllers/gym.js
@@ -90,7 +90,7 @@ class Gym extends Controller {
 			data.googleMapUrl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`
 			data.appleMapUrl = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`
 			data.wazeMapUrl = `https://www.waze.com/ul?ll=${data.latitude},${data.longitude}&navigate=yes&zoom=17`
-			data.reactMapUrl = `${this.config.general.reactMapURL}/id/gyms/${data.gymId}`
+			data.reactMapUrl = `${this.config.general.reactMapURL}id/gyms/${data.gym_id}`
 			data.rocketMadUrl = `${this.config.general.rocketMadURL}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 			data.name = this.escapeJsonString(data.name)
 			data.gymName = data.name
