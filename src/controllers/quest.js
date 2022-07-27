9c2a389c26dbfd0a52c46038da544ca99626a2c9
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-07-27 12:39:28 +0200

    Update quest.js

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -108,8 +108,8 @@ class Quest extends Controller {
 			data.googleMapUrl = `https://www.google.com/maps/search/?api=1&query=${data.latitude},${data.longitude}`
 			data.appleMapUrl = `https://maps.apple.com/maps?daddr=${data.latitude},${data.longitude}`
 			data.wazeMapUrl = `https://www.waze.com/ul?ll=${data.latitude},${data.longitude}&navigate=yes&zoom=17`
-			data.reactMapUrl = `${this.config.general.reactMapURL}id/pokestops/${data.pokestop_id}`
-			data.rocketMadUrl = `${this.config.general.rocketMadURL}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
+			data.reactMapUrl = `${this.config.general.reactMapURL}${!this.config.general.reactMapURL.endsWith('/') ? '/' : ''}id/pokestops/${data.pokestop_id}`
+			data.rocketMadUrl = `${this.config.general.rocketMadURL}${!this.config.general.rocketMadURL.endsWith('/') ? '/' : ''}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 			data.disappearTime = moment.tz(new Date(), this.config.locale.time, geoTz.find(data.latitude, data.longitude)[0].toString()).endOf('day')
 			data.applemap = data.appleMapUrl // deprecated
 			data.mapurl = data.googleMapUrl // deprecated
