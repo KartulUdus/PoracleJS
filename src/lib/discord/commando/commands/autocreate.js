bd7200aef660d020f32fe954aa065ef9d49122ef
Fabio1988 <fabio.schett@gmail.com>
2022-11-04 10:48:53 +0100

    fix autocreate deny

--- src/lib/discord/commando/commands/autocreate.js
+++ src/lib/discord/commando/commands/autocreate.js
@@ -99,12 +99,29 @@ exports.run = async (client, msg, [args]) => {
 				const roleOverwrites = []
 				for (const role of channelDefinition.roles) {
 					const allowed = []
+					const deny = []
 					roleId = await guild.roles.cache.get(format(role.id, args))
-					if (role.view) allowed.push('VIEW_CHANNEL')
-					if (role.viewHistory) allowed.push('READ_MESSAGE_HISTORY')
-					if (role.send) allowed.push('SEND_MESSAGES')
-					if (role.react) allowed.push('ADD_REACTIONS')
-					roleOverwrites.push({ id: roleId, allow: allowed })
+					if (role.view) { 
+						allowed.push('VIEW_CHANNEL')
+					} else { 
+						deny.push('VIEW_CHANNEL')
+					}
+					if (role.viewHistory) {
+						allowed.push('READ_MESSAGE_HISTORY')
+					} else { 
+						deny.push('READ_MESSAGE_HISTORY')
+					}
+					if (role.send) {
+						allowed.push('SEND_MESSAGES')
+					} else {
+						deny.push('SEND_MESSAGES')
+					}
+					if (role.react) {
+						allowed.push('ADD_REACTIONS')
+					} else {
+						deny.push('ADD_REACTIONS')
+					}
+					roleOverwrites.push({ id: roleId, allow: allowed, deny: deny })
 				}
 				channelOptions.permissionOverwrites = roleOverwrites
 			}
