837d50f5003a44e10f1cf418cc687522d1b285cd
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-01-27 10:54:10 +0100

    Update pokestop.js

--- src/controllers/pokestop.js
+++ src/controllers/pokestop.js
@@ -110,8 +110,10 @@ class Invasion extends Controller {
 			data.gruntTypeId = 0
 			if (data.incident_grunt_type) {
 				data.gruntTypeId = data.incident_grunt_type
-			} else if (data.grunt_type) {
+			} else if (data.grunt_type && (data.display_type !== 8)) {
 				data.gruntTypeId = data.grunt_type
+			} else if ((data.grunt_type == 0) && (data.display_type == 8)) {
+				data.gruntTypeId = 352
 			}
 
 			data.gruntTypeColor = 'BABABA'
@@ -124,7 +126,7 @@ class Invasion extends Controller {
 
 			// Only enough to match for query
 
-			if (data.gruntTypeId) {
+			if (data.gruntTypeId && (data.gruntTypeId !== 352)) {
 				data.gender = 0
 				data.gruntName = 'Grunt'
 				data.gruntType = 'Mixed'
@@ -135,6 +137,14 @@ class Invasion extends Controller {
 					data.gender = gruntType.gender
 					data.gruntType = gruntType.type
 				}
+			}
+
+			// Kecleon invasion
+			if (data.gruntTypeId && (data.gruntTypeId == 352)) {
+				data.gender = 0
+				data.gruntName = 'Kecleon'
+				data.gruntType = 'Kecleon'
+				data.gruntRewards = ''
 			}
 
 			const whoCares = data.poracleTest ? [{
@@ -164,9 +174,16 @@ class Invasion extends Controller {
 
 			setImmediate(async () => {
 				try {
-					if (this.imgUicons) data.imgUrl = await this.imgUicons.invasionIcon(data.gruntTypeId) || this.config.fallbacks?.imgUrlPokestop
-					if (this.imgUiconsAlt) data.imgUrlAlt = await this.imgUiconsAlt.invasionIcon(data.gruntTypeId) || this.config.fallbacks?.imgUrlPokestop
-					if (this.stickerUicons) data.stickerUrl = await this.stickerUicons.invasionIcon(data.gruntTypeId)
+					if data.gruntTypeId == 352 {
+						data.shinyPossible = this.shinyPossible.isShinyPossible(data.gruntTypeId, 0)
+						if (this.imgUicons) data.imgUrl = await this.imgUicons.pokemonIcon(data.gruntTypeId, 0, 0, data.gender, 0, data.shinyPossible && this.config.general.requestShinyImages) || this.config.fallbacks?.imgUrl
+						if (this.imgUiconsAlt) data.imgUrlAlt = await this.imgUiconsAlt.pokemonIcon(data.gruntTypeId, 0, 0, data.gender, 0, data.shinyPossible && this.config.general.requestShinyImages) || this.config.fallbacks?.imgUrl
+						if (this.stickerUicons) data.stickerUrl = await this.stickerUicons.pokemonIcon(data.gruntTypeId, 0, 0, data.gender, 0, data.shinyPossible && this.config.general.requestShinyImages)
+					} else {
+						if (this.imgUicons) data.imgUrl = await this.imgUicons.invasionIcon(data.gruntTypeId) || this.config.fallbacks?.imgUrlPokestop
+						if (this.imgUiconsAlt) data.imgUrlAlt = await this.imgUiconsAlt.invasionIcon(data.gruntTypeId) || this.config.fallbacks?.imgUrlPokestop
+						if (this.stickerUicons) data.stickerUrl = await this.stickerUicons.invasionIcon(data.gruntTypeId)
+					}
 
 					const geoResult = await this.getAddress({ lat: data.latitude, lon: data.longitude })
 					const jobs = []
@@ -200,7 +217,7 @@ class Invasion extends Controller {
 						require('./common/weather').setGameWeather(data, translator, this.GameData, this.emojiLookup, platform, currentCellWeather)
 
 						// full build
-						if (data.gruntTypeId) {
+						if (data.gruntTypeId && (data.gruntTypeId !== 352)) {
 							data.gender = 0
 							data.gruntName = translator.translate('Grunt')
 							data.gruntType = translator.translate('Mixed')
