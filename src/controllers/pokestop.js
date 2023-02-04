88ba23fee90ff3ed9d158445b07923dcdb9592c6
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-02-04 20:32:28 +0100

    Update pokestop.js

--- src/controllers/pokestop.js
+++ src/controllers/pokestop.js
@@ -109,16 +109,16 @@ class Invasion extends Controller {
 
 			data.gruntTypeId = 0
 			if (data.incident_grunt_type && (data.incident_grunt_type !== 352)) {
-				data.gruntTypeId = data.incident_grunt_type
+				data.gruntTypeId = data.incident_grunt_type // MAD Master Grunt
 			} else if (data.character_display && (data.incident_display_type == 1)) {
-				data.gruntTypeId = data.character_display
+				data.gruntTypeId = data.character_display // MAD Async Grunt
 			} else if (data.grunt_type && (data.display_type == 1)) {
-				data.gruntTypeId = data.grunt_type
+				data.gruntTypeId = data.grunt_type // RDM Grunt
 			} else if (data.incident_grunt_type == 352) {
-				data.grunt_type = 0
+				data.grunt_type = 0 // MAD Master EventStop
 				data.display_type = 8
-			} else if (data.character_display  && (data.incident_display_type != 1)) {
-				data.grunt_type = 0
+			} else if (data.incident_display_type != 1) {
+				data.grunt_type = 0 // MAD Async EventStop
 				data.display_type = data.incident_display_type
 			}
 
