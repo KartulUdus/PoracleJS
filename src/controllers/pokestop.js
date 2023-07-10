c9a9317079868528a2aadf11a96ea9131597f14b
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-07-10 13:20:33 +0200

    Update pokestop.js

--- src/controllers/pokestop.js
+++ src/controllers/pokestop.js
@@ -145,6 +145,13 @@ class Invasion extends Controller {
 				data.gruntName = data.displayTypeId && this.GameData.utilData.pokestopEvent[data.displayTypeId] ? this.GameData.utilData.pokestopEvent[data.displayTypeId] : ''
 				data.gruntType = data.displayTypeId && this.GameData.utilData.pokestopEvent[data.displayTypeId] ? this.GameData.utilData.pokestopEvent[data.displayTypeId].toLowerCase() : ''
 				data.gruntRewards = ''
+				if (data.displayTypeId === 7) {
+					data.gruntTypeColor = 'F9E418'
+				} else if (data.displayTypeId === 8) {
+					data.gruntTypeColor = 'B3CA78'
+				} else if (data.displayTypeId === 9) {
+					data.gruntTypeColor = '03AEB6'
+				}
 			}
 
 			const whoCares = data.poracleTest ? [{
