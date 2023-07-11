cb8a132f574b2bb4a2318de02cb17f66bcef6420
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-07-11 13:14:23 +0200

    Update invasion.js

--- src/lib/discord/commando/commands/invasion.js
+++ src/lib/discord/commando/commands/invasion.js
@@ -1,7 +1,7 @@
 const PoracleDiscordMessage = require('../../poracleDiscordMessage')
 const PoracleDiscordState = require('../../poracleDiscordState')
 
-const commandLogic = require('../../../poracleMessage/commands/invasion')
+const commandLogic = require('../../../poracleMessage/commands/incident')
 
 exports.run = async (client, msg, command) => {
 	const pdm = new PoracleDiscordMessage(client, msg)
