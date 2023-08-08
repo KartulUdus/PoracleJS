7263b0cc56b82710e36f0888deb9700c90b83b15
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-08-08 13:10:45 +0200

    QuestFix Vol2

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -387,10 +387,7 @@ class Quest extends Controller {
 		if (item.quest_task && !this.config.general.ignoreMADQuestString) {
 			str = item.quest_task
 		} else {
-			if (item.title) {
-				item.quest_title = item.title
-			}
-			const questinfo = `quest_title_${item.title}`
+			const questinfo = `quest_title_${item.title.toLowerCase()}`
 			const questTitle = this.GameData.translations[language].questTitles
 			if (item.title) {
 				try {
