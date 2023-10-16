d7aad42f9c45d5d9f591f8f9d47e44b954c71c35
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-10-16 11:21:32 -0400

    fix: koji config reading

--- src/util/koji.js
+++ src/util/koji.js
@@ -1,28 +1,11 @@
 const fs = require('fs')
 const { resolve } = require('path')
 const fetch = require('node-fetch')
-const json5 = require('json5')
+const config = require('config')
 
 const { log } = require('../lib/logger')
 
-const parseConfigSafe = () => {
-	let config = {}
-	try {
-		if (fs.existsSync(resolve(__dirname, '../../config/local.json'))) {
-			const string = fs.readFileSync(resolve(__dirname, '../../config/local.json')).toString()
-			config = json5.parse(string)
-		} else {
-			log.warn('[KŌJI] Local.json was not found, skipping')
-		}
-		return config
-	} catch (e) {
-		log.error('[KŌJI] Could not parse local config', e)
-		return config
-	}
-}
-
 const getKojiFences = async () => {
-	const config = parseConfigSafe()
 	try {
 		if (config?.geofence?.kojiOptions?.bearerToken) {
 			const fences = Array.isArray(config.geofence?.path)
