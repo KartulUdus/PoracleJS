353fdd1dae58af020a11a0d765caa9c09671c5ba
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-11-03 13:07:51 -0400

    feat: Koji loading fix & reloading API (#877)
    * fix: koji config reading
    
    * fix: geofence file watching
    
    * feat: add reload api
    
    * refactor: simplify

--- src/routes/apiGeofence.js
+++ src/routes/apiGeofence.js
@@ -1,4 +1,5 @@
 const geofenceTileGenerator = require('../lib/geofenceTileGenerator')
+const { getKojiFences } = require('../util/koji')
 
 module.exports = async (fastify, options, next) => {
 	fastify.get('/api/geofence/:area/map', options, async (req) => {
@@ -225,10 +226,10 @@ module.exports = async (fastify, options, next) => {
 			const outPath = []
 			if (inGeofence.multipath) {
 				for (let j = 0; j < inGeofence.multipath.length; j++) {
-					const path = inGeofence.multipath[j]
+					const geofencePath = inGeofence.multipath[j]
 					const outSubPath = []
-					for (let k = 0; k < path.length; k++) {
-						const coord = path[k]
+					for (let k = 0; k < geofencePath.length; k++) {
+						const coord = geofencePath[k]
 						outSubPath.push([coord[1], coord[0]])
 					}
 					if (outSubPath.at(-1)[0] !== outSubPath[0][0] || outSubPath.at(-1)[1] !== outSubPath[0][1]) {
@@ -253,6 +254,32 @@ module.exports = async (fastify, options, next) => {
 			status: 'ok',
 			geoJSON: outGeoJSON,
 		}
+	})
+
+	fastify.get('/api/geofence/reload', options, async (req) => {
+		fastify.logger.info(`API: ${req.ip} ${req.routeOptions.method} ${req.routeOptions.url}`)
+
+		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) {
+			return {
+				webserver: 'unhappy',
+				reason: `ip ${req.ip} not in whitelist`,
+			}
+		}
+		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) {
+			return {
+				webserver: 'unhappy',
+				reason: `ip ${req.ip} in blacklist`,
+			}
+		}
+
+		const secret = req.headers['x-poracle-secret']
+		if (!secret || !fastify.config.server.apiSecret || secret !== fastify.config.server.apiSecret) {
+			return { status: 'authError', reason: 'incorrect or missing api secret' }
+		}
+
+		await getKojiFences()
+
+		return { status: 'ok' }
 	})
 
 	next()
