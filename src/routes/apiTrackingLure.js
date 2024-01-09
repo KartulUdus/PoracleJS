c47d8819583f78c7a89b2dea55c3974a924dcf48
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2024-01-09 11:44:27 +0100

    Update apiTrackingLure.js

--- src/routes/apiTrackingLure.js
+++ src/routes/apiTrackingLure.js
@@ -2,7 +2,7 @@ const { diff } = require('deep-object-diff')
 
 const trackedCommand = require('../lib/poracleMessage/commands/tracked')
 
-module.exports = async (fastify, options, next) => {
+module.exports = async (fastify, options) => {
 	fastify.get('/api/tracking/lure/:id', options, async (req) => {
 		fastify.logger.info(`API: ${req.ip} ${req.routeOptions.method} ${req.routeOptions.url}`)
 
@@ -211,6 +211,4 @@ module.exports = async (fastify, options, next) => {
 			status: 'ok',
 		}
 	})
-
-	next()
 }
