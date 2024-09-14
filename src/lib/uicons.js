8fa7aa8be1325d40f7ef9b81146214770d60b763
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-09-13 22:13:40 -0400

    uicons.js 2.0

--- src/lib/uicons.js
+++ src/lib/uicons.js
@@ -1,14 +1,21 @@
-const axios = require('axios')
+// @ts-check
+
+const { default: axios } = require('axios')
 const { Mutex } = require('async-mutex')
 const { UICONS } = require('uicons.js')
 
 const mutex = new Mutex()
 
-const uiconsIndex = /** @type {Record<string, UICONS>} */ ({})
+const uiconsIndex = /** @type {Record<string, UICONS | null>} */ ({})
 const lastRetrieved = /** @type {Record<string, number>} */ ({})
 
 const maxAge = 60 * 60 * 1000	// 1 hour
 
+/**
+ * @param {any} log
+ * @param {string} baseUrl
+ * @returns {Promise<UICONS | null>}
+ */
 async function getAvailableIcons(log, baseUrl) {
 	let currentSet = uiconsIndex[baseUrl]
 	let lastRetrievedDate = lastRetrieved[baseUrl]
@@ -42,9 +49,7 @@ async function getAvailableIcons(log, baseUrl) {
 							break
 						}
 						case 200: {
-							const uicons = new UICONS(baseUrl)
-							uicons.init(response.data)
-							uiconsIndex[baseUrl] = uicons
+							uiconsIndex[baseUrl] = new UICONS(baseUrl).init(response.data)
 							currentSet = uiconsIndex[baseUrl]
 							break
 						}
@@ -66,6 +71,12 @@ async function getAvailableIcons(log, baseUrl) {
 }
 
 class Uicons {
+	/**
+	 * @param {string} url
+	 * @param {string} [imageType]
+	 * @param {Console} [log]
+	 * @param {boolean} [fallback]
+	 */
 	constructor(url, imageType, log, fallback) {
 		this.url = url.endsWith('/') ? url.slice(0, -1) : url
 		this.imageType = imageType || 'png'
@@ -78,81 +89,86 @@ class Uicons {
 		return !!currentSet
 	}
 
-	async pokemonIcon(pokemonId, form = 0, evolution = 0, gender = 0, costume = 0, alignment = 0, shiny = false) {
+	async pokemonIcon(pokemonId = 0, form = 0, evolution = 0, gender = 0, costume = 0, alignment = 0, shiny = false, bread = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
-		if (currentSet) return currentSet.pokemon(pokemonId, form, evolution, gender, costume, alignment, shiny)
+		if (currentSet) return currentSet.pokemon(pokemonId, evolution, form, costume, gender, alignment, bread, shiny)
 		if (this.fallback) return `${this.url}/pokemon_icon_${pokemonId.toString().padStart(3, '0')}_${form ? form.toString() : '00'}${evolution > 0 ? `_${evolution.toString()}` : ''}.${this.imageType}`
 		return null
 	}
 
-	async eggIcon(level, hatched = false, ex = false) {
+	async eggIcon(level = 0, hatched = false, ex = false) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		if (currentSet) return currentSet.raidEgg(level, hatched, ex)
 		if (this.fallback) return `${this.url}/egg${level}.${this.imageType}`
 		return null
 	}
 
-	async invasionIcon(gruntType) {
+	async invasionIcon(gruntType = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		return currentSet ? currentSet.invasion(gruntType) : null
 	}
 
-	async typeIcon(typeId) {
+	async typeIcon(typeId = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		return currentSet ? currentSet.type(typeId) : null
 	}
 
-	async teamIcon(teamId) {
+	async teamIcon(teamId = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		return currentSet ? currentSet.team(teamId) : null
 	}
 
-	async rewardItemIcon(itemId) {
+	async rewardItemIcon(itemId = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		if (currentSet) return currentSet.reward('item', itemId)
 		if (this.fallback) return `${this.url}/rewards/reward_${itemId}_1.${this.imageType}`
 		return null
 	}
 
-	async rewardStardustIcon(amount) {
+	async rewardStardustIcon(amount = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		if (currentSet) return currentSet.reward('stardust', amount)
 		if (this.fallback) return `${this.url}/rewards/reward_stardust.${this.imageType}`
 		return null
 	}
 
-	async rewardMegaEnergyIcon(pokemonId, amount) {
+	async rewardMegaEnergyIcon(pokemonId = 0, amount = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		if (currentSet) return currentSet.reward('mega_resource', pokemonId, amount)
 		if (this.fallback) return `${this.url}/rewards/reward_mega_energy_${pokemonId}.${this.imageType}`
 		return null
 	}
 
-	async rewardCandyIcon(pokemonId, amount) {
+	async rewardCandyIcon(pokemonId = 0, amount = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		if (currentSet) return currentSet.reward('candy', pokemonId, amount)
 		if (this.fallback) return `${this.url}/rewards/reward_candy_${pokemonId}.${this.imageType}`
 		return null
 	}
 
-	async rewardXlCandyIcon(pokemonId, amount) {
+	async rewardXlCandyIcon(pokemonId = 0, amount = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
 		return currentSet ? currentSet.reward('xl_candy', pokemonId, amount) : null
 	}
 
-	async gymIcon(teamId, trainerCount = 0, inBattle = false, ex = false) {
+	async gymIcon(teamId = 0, trainerCount = 0, inBattle = false, ex = false, ar = false, power = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
-		return currentSet ? currentSet.gym(teamId, trainerCount, inBattle, ex) : null
+		return currentSet ? currentSet.gym(teamId, trainerCount, inBattle, ex, ar, power) : null
 	}
 
-	async weatherIcon(weatherId) {
+	async weatherIcon(weatherId = 0, severity = 0, timeOfDay = 'day') {
 		const currentSet = await getAvailableIcons(this.log, this.url)
-		return currentSet ? currentSet.weather(weatherId) : null
+		return currentSet ? currentSet.weather(weatherId, severity, timeOfDay) : null
 	}
 
-	async pokestopIcon(lureId = 0, invasionActive = false, incidentDisplayType = 0, questActive = false) {
+	async pokestopIcon(lureId = 0, invasionActive = false, incidentDisplayType = 0, questActive = false, ar = false, power = 0) {
 		const currentSet = await getAvailableIcons(this.log, this.url)
-		return currentSet ? currentSet.pokestop(lureId, 0, incidentDisplayType, invasionActive, questActive) : null
+		return currentSet ? currentSet.pokestop(lureId, incidentDisplayType || !!invasionActive, questActive, ar, power) : null
+	}
+
+	async stationIcon(active = false) {
+		const currentSet = await getAvailableIcons(this.log, this.url)
+		return currentSet ? currentSet.station(active) : null
 	}
 }
 
