766416aa550ef15431460c2e7ec832a15995f4d5
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2025-08-26 12:29:17 +0200

    Update apiTrackingQuest.js

--- src/routes/apiTrackingQuest.js
+++ src/routes/apiTrackingQuest.js
@@ -135,14 +135,14 @@ module.exports = async (fastify, options) => {
 			if ((alreadyPresent.length + updates.length + insert.length) > 50) {
 				message = translator.translateFormat('I have made a lot of changes. See {0}{1} for details', '!', /* util.prefix, */ translator.translate('tracked'))
 			} else {
-				for (const lure of alreadyPresent) {
-					message = message.concat(translator.translate('Unchanged: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const i of alreadyPresent) {
+					message = message.concat(translator.translate('Unchanged: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, i), '\n')
 				}
-				for (const lure of updates) {
-					message = message.concat(translator.translate('Updated: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const i of updates) {
+					message = message.concat(translator.translate('Updated: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, i), '\n')
 				}
-				for (const lure of insert) {
-					message = message.concat(translator.translate('New: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const i of insert) {
+					message = message.concat(translator.translate('New: '), trackedCommand.questRowText(fastify.config, translator, fastify.GameData, i), '\n')
 				}
 			}
 
