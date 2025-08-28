0b44c5132f9436cffb97cee487244a4c38dcb8f1
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2025-08-29 00:26:37 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -775,7 +775,6 @@ async function processOne(hook) {
 				await processHook(hook)
 				break
 			}
-
 			case 'nest': {
 				if (config.general.disableNest) {
 					fastify.controllerLog.debug(`${hook.message.nest_id}: Nest was received but set to be ignored in config`)
@@ -797,7 +796,6 @@ async function processOne(hook) {
 				await processHook(hook)
 				break
 			}
-
 			case 'max_battle': {
 				if (config.general.disableMaxBattle) {
 					fastify.controllerLog.debug(`${hook.message.id}: MaxBattle was received but set to be ignored in config`)
@@ -819,7 +817,6 @@ async function processOne(hook) {
 				await processHook(hook)
 				break
 			}
-
 			case 'weather': {
 				if (config.general.disableWeather) break
 				fastify.webhooks.info(`weather ${JSON.stringify(hook.message)}`)
